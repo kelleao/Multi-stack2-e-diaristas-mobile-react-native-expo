d613efe087da33524e31c2af8f78df96ac1da767
Kel Leão <raquel179@gmail.com>
2022-12-08 13:23:13 -0300

    Exibir do usuario rating

--- src/pages/encontrar-diarista.tsx
+++ src/pages/encontrar-diarista.tsx
@@ -1,8 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import PageTitle from 'ui/components/inputs/Button/data-display/PageTitle/PageTitle';
+import UserInformation from 'ui/components/inputs/Button/data-display/UserInformation/UserInformation';
+import TextInputMask from 'ui/components/inputs/TextInputMask/TextInputMask';
 import { RootStackParamList } from 'ui/router/Router';
 
 type NavigationProp = StackNavigationProp<
@@ -12,11 +14,24 @@ type NavigationProp = StackNavigationProp<
 
 const EncontrarDiarista: React.FC = () => {
     const navigation = useNavigation<NavigationProp>();
+    const [text, setText] = useState('');
 
     return (
         <View>
             <Text>Encontrar Diaristas</Text>
-            <PageTitle title={'titulo'} />
+            <PageTitle title={'titulo'} subtitle={'AB djfdjfldjfl'} />
+            <TextInputMask
+                value={text}
+                onChangeText={setText}
+                label={'Nome'}
+                mask={'99.999-999'}
+            />
+            <UserInformation
+                name={'Raquel Leão'}
+                picture={'https://github.com/kelleao.png'}
+                rating={4}
+                description={'Belo Horizonte'}
+            />
         </View>
     );
 };
